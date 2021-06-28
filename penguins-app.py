1276c609596219cf95d88f09aa4b8b1155a2a189
jnlinao <51682976+jnlinao@users.noreply.github.com>
2021-06-28 13:55:29 -0700

    Update penguins-app.py
    change code

--- penguins-app.py
+++ penguins-app.py
@@ -3,12 +3,19 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-st.write("""
-# Penguin Prediction App
+st.write(
+	"""
+	# ChangeGear Incident Classifier
+	Write or paste your ticket description for the machine learning algorithm to predict what IT Department to assign your incident to. 
+	
+	"""
 
-This app predicts the **Palmer Penguin** species!
+	)
 
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
-""")
-
+#receive input text from user
+st.subheader('Enter Incident Summary')
+input_field = st.text_input("")
+ticket_text = [input_field]
